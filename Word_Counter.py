4cb1f0730898b5138535874bab65b9be7971d1df
buim <buim@educ.college-montreal.qc.ca>
2023-09-12 16:19:50 -0400

    j'ai ajouté un paramètre

--- Word_Counter.py
+++ Word_Counter.py
@@ -1,7 +1,7 @@
 print("Ce programme compte le nombre de mots présent dans une phrase de votre choix\n")
 phrase = input("Veuillez écrire la phrase de que vous souhaitez.\n")
 
-def word_counter():
+def word_counter(phrase):
     count = len(phrase .split())
     return count
-print("votre phrase contient %d mots"%word_counter())
+print("votre phrase contient %d mots"%word_counter(phrase))
