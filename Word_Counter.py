722319d3439935d8094b073cbdf8acf39f8aa222
buim <buim@educ.college-montreal.qc.ca>
2023-09-12 16:10:59 -0400

    je m'étais trompé, c'est nombre de mots pas de lettres, j'ai changé pour le nombre de mots.

--- Word_Counter.py
+++ Word_Counter.py
@@ -1,8 +1,7 @@
-print("Ce programme compte le nombre de charactère présent dans le mot ou phrase de votre choix\n")
-mot = input("Veuillez écrire le mot ou phrase de que vous souhaitez.\n")
+print("Ce programme compte le nombre de mots présent dans une phrase de votre choix\n")
+phrase = input("Veuillez écrire la phrase de que vous souhaitez.\n")
 
 def word_counter():
-    count = len(mot)
+    count = len(phrase .split())
     return count
-print(word_counter())
-print("votre mot ou phrase contient %d lettres"%word_counter())
+print("votre phrase contient %d mots"%word_counter())
