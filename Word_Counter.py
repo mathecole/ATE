c0e386b48add03c79063fd9b20bf89aa30864ab1
You won't se me coming <144698019+mathecole@users.noreply.github.com>
2023-09-18 23:40:50 -0400

    ajout de notes

--- Word_Counter.py
+++ Word_Counter.py
@@ -1,7 +1,11 @@
 print("Ce programme compte le nombre de mots présent dans une phrase de votre choix\n")
+#introduit l'utilisateur
 phrase = input("Veuillez écrire la phrase de que vous souhaitez.\n")
+#détenir la phrase
 
 def word_counter(phrase):
     count = len(phrase .split())
+    #compte le nombre de phrases dans la variable
     return count
 print("votre phrase contient %d mots"%word_counter(phrase))
+#envoi le nombre de phrases à l'utilisateur
