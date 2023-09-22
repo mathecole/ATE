712b7ea78c8326b4576fbb6359c62f37fa49d53c
buim <buim@educ.college-montreal.qc.ca>
2023-09-22 09:49:26 -0400

    Mon nom et celui du programme

--- Word_Counter.py
+++ Word_Counter.py
@@ -1,11 +1,18 @@
+# Mathéo Vinh Bui
+# Tp1 Word Counter
+
+
 print("Ce programme compte le nombre de mots présent dans une phrase de votre choix\n")
-#introduit l'utilisateur
+# introduit l'utilisateur
 phrase = input("Veuillez écrire la phrase de que vous souhaitez.\n")
-#détenir la phrase
+# détenir la phrase
+
 
 def word_counter(phrase):
-    count = len(phrase .split())
-    #compte le nombre de phrases dans la variable
+    count = len(phrase.split())
+    # compte le nombre de phrases dans la variable
     return count
-print("votre phrase contient %d mots"%word_counter(phrase))
-#envoi le nombre de phrases à l'utilisateur
+
+
+print("votre phrase contient %d mots" % word_counter(phrase))
+# envoi le nombre de phrases à l'utilisateur
