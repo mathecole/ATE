563018c1a557d0262a0e1e710921ab003de605ea
mathecole <144698019+mathecole@users.noreply.github.com>
2023-09-22 16:03:10 +0000

    Programme devinette fonctionnel

--- TP2.py
+++ TP2.py
@@ -1,10 +1,15 @@
 import random
 essais = 0
 nombre_mystere = random.randint(0,1000)
-
-nombre_guess = input("Essaye de deviner mot nombre mystère, il se situe entre 0 et 100 ")
+guess = []
+print(nombre_mystere)
+nombre_guess = int(input("Essaye de deviner mon nombre mystère, il se situe entre 0 et 1000 "))
 while (nombre_mystere != nombre_guess):
-    essais += 1
+  essais += 1
+  guess.append(nombre_guess)
+  nombre_guess = int(input("non c'est pas %s, btw ça fait %s fois que t'essaye"%(nombre_guess, essais)))
+  
+  if nombre_guess in guess:
+    print("ta déja essayer %s"%nombre_guess)
 
-    nombre_guess = input("non c'est pas %s, btw ça fait %s fois que t'essaye"%(nombre_guess, essais))
-print("fuck you t'es une #### de #######, comment tu savais c'était %s?"%nombre_mystere)
+print("Tu l'as eu c'était %s."%nombre_mystere)
