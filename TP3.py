e7e5f034144c3f69f6cf38242628fd51782981a9
buim <buim@educ.college-montreal.qc.ca>
2023-10-12 15:38:35 -0400

    code fonctionnel

--- TP3.py
+++ TP3.py
@@ -11,29 +11,35 @@ def interaction(force, combats, adversaire):
     global defaite
     global victoire
     global vie
-    action=0
-    while(action != 1 or action != 2 or action != 3 or action != 4):
+    action = 0
+    while True:
         print("\n Adversaire: %s\n Force de l'adversaire: %s\n Niveau de vie: %s\n Combats %s: victoires %s et défaites %s" % (adversaire, force, vie, combats, victoire, defaite))
         action = input(str("Que voulez-vous faire ?\n1- Combattre l'adversaire\n2- Fuir\n3- Règles\n4- Quitter\n"))
         if action == "1":
             print("roulement de dés...")
             attaque = random.randint(1,6)
             print("vous avez roulez %s"%attaque)
-            if attaque < force:
+            if attaque <= force:
                 vie -= force
                 print("vous avez perdu %s points de vie, vous avez maintenant %s points de vie\n"%(force, vie))
                 defaite += 1
-            elif attaque > force:
+            else:
                 vie += force
                 victoire += 1
                 print("vous avez gagnez %s points de vie, vous avez maintenant %s points de vie"%(force,vie))
+            break
         elif action == "2":
             print("Vous êtes un vrai lâche, vous avez fuit. Pour votre manque de bravour, vous avez reçu une défaite.")
             defaite += 1
             print("Vous possèdez maintenant %s défaites"%defaite)
+            break
         elif action == "3":
             print(" Voici les règles du jeu.\n Vous commencez la partie avec 20 points de vies, vôtre tâche sera d'accumuler")
-            print(" le plus de victoires possibles, pour vaincre ces adversaires vous allez devoir")
+            print(" le plus de victoires possibles, pour vaincre ces adversaires vous allez devoir rouler un dé si le chiffre")
+            print(" est supérieur à la force de l'adversaire, vous gagnez une victoire et s'il est égal ou inférieur à celui-ci, vous perdez.")
+            print(" Vous pouvez ausi vous enfuir si vous ne souhaitez pas faire face à l'énnemi, ceci vous donnera par contre un défaite.")
+            print(" Vous pouvez aussi demander les règles, où ce message apparêtra. Lorsque votre niveau de vie atteint 0 la partie se termine.")
+            break
         elif action == "4":
             print("Le code va s'arreter")
             quit()
@@ -41,6 +47,6 @@ def interaction(force, combats, adversaire):
             print("Tu ne peux pas repondre autre que 1, 2, 3 ou 4, reessaye")
 while(vie>0):
     adversaire += 1
-    force = random.randint(1, 5)
+    force = random.randint(1, 4)
     interaction(force, combats, adversaire)
     combats +=1
