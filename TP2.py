6d6e27a793190115d87ee6b05a262eef32f75c82
buim <buim@educ.college-montreal.qc.ca>
2023-10-20 10:44:55 -0400

    commentaires ajoutés

--- TP2.py
+++ TP2.py
@@ -1,12 +1,14 @@
 import random
 jouer="o"
+
+#Cette boucle while s'occupe de démarrer une partie
 while(jouer == "o"):
   essais = 0
   nombre_mystere = random.randint(0,1000)
   guess = []
-  nombre_guess = int(input("Essaye de deviner mon nombre mystère, il se situe entre 0 et 1000 "))
+  nombre_guess = int(input("Essayez de deviner mon nombre mystère, il se situe entre 0 et 1000 "))
 
-
+#Cette boucle analyse l'input de l'utilisateur
   while (nombre_mystere != nombre_guess):
     if(nombre_guess > nombre_mystere):
      print("le nombre est plus petit")
@@ -15,11 +17,19 @@ while(jouer == "o"):
       print("le nombre est plus grand")
 
     essais +=1
-    guess.append(nombre_guess)
-    nombre_guess = int(input("non c'est pas %s, btw ça fait %s fois que t'essaye"%(nombre_guess, essais)))
+    nombre_guess = int(input("non ce n'est pas %s, il fait %s fois que vous essayez"%(nombre_guess, essais)))
     if nombre_guess in guess:
-      print("ta déja essayer %s"%nombre_guess)
+      print("vous déja essayez %s"%nombre_guess)
+    guess.append(nombre_guess)
 
   print("Tu l'as eu c'était %s."%nombre_mystere)
-  jouer = input("voulez vous rejouer? o/n")
+#Cette boucle s'occupe de terminer/recommencer une partie
+  while True:
+    jouer = input("voulez vous rejouer? o/n")
+    if jouer == "o":
+      break
+    elif jouer == "n":
+      quit()
+    else:
+      print("Vous n'avez pas inséré o/n, réessasyez")
   guess.clear()
