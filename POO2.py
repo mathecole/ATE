81945999d581a359b85bc4031fcda11221cb0cec
buim <buim@educ.college-montreal.qc.ca>
2024-02-01 14:46:26 -0500

    avance TP4

--- POO2.py
+++ POO2.py
@@ -1,4 +1,4 @@
-import random
+    import random
 def diceroll():
     dice = []
     dice.append(random(4, 6))
--- TP4.py
+++ TP4.py
@@ -2,29 +2,80 @@ import arcade
 import random
 import time
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 960
+SCREEN_HEIGHT = 540
 
 COLORS = []
 
 
+class Balle(self, x, y):
+    def __init__(self):
+        self.x = x
+        self.y = y
+        self.change_x = random.randint(-4, 4)
+        self.change_y = random.randint(-4, 4)
+        self.rayon = random.randint(6, 15)
+        self.color = random.choice(Colours)
+    def on_draw(self):
+        arcade.draw_criclefilled(self.x, self.y, self.rayon, color = self.color)
+    def on_update(self):
+        self.x += self.change_x
+        self.y += self.change_y
+        if self.x > SCREEN_WIDTH - self.rayon:
+            self.change_y *= -1
+        if self.y > SCREEN_HEIGHT -self.rayon:
+            self.change_y *= -1
+
+
+class Rectangle(self, x, y):
+    def __init__(self):
+        self.x = x
+        self.y = y
+        self.width = random.randint(4,6)
+        self.height = random.randint(6,12)
+        self.change_x = random.randint(-4, 4)
+        self.change_y = random.randint(-4, 4)
+        self.color = random.choice(Colours)
+        self.angle = random.uniform(0,90)
+
+    def on_update(self):
+        self.x += self.change_x
+        self.y += self.change_y
+        if self.x > SCREEN_WIDTH - self.width:
+            self.change_x *= -1
+        if self.Y > SCREEN_HEIGHT - self.height:
+            self.change_y *= -1
+
+
+    def on_draw(self):
+        arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, color = self.color, tilt_angle=self.angle)
 class MyGame(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Exercice #1")
-        pass
+        self.liste_balles = []
+        self.liste_rectangles = []
+
 
     def setup(self):
         pass
 
     def on_draw(self):
         arcade.start_render()
-        x = 400
-        y = 300
+        for i in self.liste_balles:
+            i.on_draw()
+        for i in self.liste_rectangles
+            i.on_draw()
 
-        arcade.draw_circle_filled(400, 300, 30, (255, 54, 34))
-    def on_update(self):
-        xchange =10
+    def on_update(self, float, delta_time):
         pass
+
+    def on_mouse_press(self, x, y, modifiers, button):
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            balle_child = Balle(x, y)
+            self.liste_balles.append(balle_child)
+        if button == arcade.MOUSE_BUTTON_RIGHT:
+            rectangle_child = Rectangle(x, y)
+            self.liste_rectangles.append(rectangle_child)
 
 
 def main():
